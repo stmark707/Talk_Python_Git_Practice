34e1119f4a35e7c78c79dc2239859923f9f9ff5c
stmark707 <stmark707@gmail.com>
2022-05-19 21:52:37 -0700

    Added some funny jokes that change with a refresh.

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import random
 app = Flask(__name__)
 
+jokes = [
+    "Relationship status? I'll leave the relations to the database.",
+    "How do you get the code for the bank vault? You checkout their branch.",
+    "Why did the security conscious engineer refuse to pay their dinner bill? Because they could not verify the checksum."
+]
+
 @app.route("/") #Video uses a get method but this did not work on our system.
-def hello_world():
-    return "<p> Hello, World! </p>"
+def tell_a_Joke():
+    joke = random.choice(jokes)
+    return f"<p>{joke}</p>"
