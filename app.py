f0d4794dda8c5070b52ef4bdcf1725c90c8cc2a0
stmark707 <stmark707@gmail.com>
2022-05-21 04:27:14 -0700

    Code clean up and refactoring.

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+import flask
 from flask import Flask
 import random
+from data import jokes
+
 app = Flask(__name__)
 
-jokes = [
-    "Relationship status? I'll leave the relations to the database.",
-    "How do you get the code for the bank vault? You checkout their branch.",
-    "Why did the security conscious engineer refuse to pay their dinner bill? Because they could not verify the checksum."
-]
 
 @app.route("/") #Video uses a get method but this did not work on our system.
 def tell_a_Joke():
     joke = random.choice(jokes)
-    return f"<p>{joke}</p>"
+    
+    return flask.render_template('joke.html', joke_text=joke)
+    
+    #return f"<p>{joke}</p>"
+    
