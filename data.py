728514d51f8f61160f9c8b236a6db07864fa04b7
stmark707 <stmark707@gmail.com>
2022-05-21 04:27:29 -0700

    Added a data layer.

---
+++ data.py
@@ -0,0 +1,5 @@
+jokes = [
+    "Relationship status? I'll leave the relations to the database.",
+    "How do you get the code for the bank vault? You checkout their branch.",
+    "Why did the security conscious engineer refuse to pay their dinner bill? Because they could not verify the checksum."
+]
